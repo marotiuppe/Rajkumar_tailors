e879322dedcbd26dcd3f7d0327a54b76ac881243
Maroti Uppe <marotiuppe891@gmail.com>
2023-08-28 01:13:14 +0530

    Added main navigation components
    Implemented the following changes:
    1. Added main navigation components: About, Process, Work, Products, Features, and Contact.
    2. Removed header components and unnecessary files.

--- rajkumar-tailors-ui/src/app/app.module.ts
+++ rajkumar-tailors-ui/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PreloaderComponent } from './preloader/preloader.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { AboutComponent } from './about/about.component';
+import { ProcessComponent } from './process/process.component';
+import { WorkComponent } from './work/work.component';
+import { ProductsComponent } from './products/products.component';
+import { FeatureComponent } from './feature/feature.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
-    PreloaderComponent
+    PreloaderComponent,
+    NavigationComponent,
+    CarouselComponent,
+    AboutComponent,
+    ProcessComponent,
+    WorkComponent,
+    ProductsComponent,
+    FeatureComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ rajkumar-tailors-ui/src/app/process/process.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-process',
+  templateUrl: './process.component.html',
+  styleUrls: ['./process.component.css']
+})
+export class ProcessComponent {
+
+}
