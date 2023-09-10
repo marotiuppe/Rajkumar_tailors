43758f5bea7427635006779ec4dffef6fc0421a2
Maroti Uppe <marotiuppe891@gmail.com>
2023-09-10 18:37:47 +0530

    Add footer, about, and feature components
    This commit introduces three new components:
    - Footer component for site-wide footer content.
    - About component for information about the Rajkumar Tailor.
    - Feature component to showcase Rajkumar Tailor features.
    
    Changes Made:
    - Created the footer, about, and feature components.
    - Added basic structure and content to each component.

--- rajkumar-tailor-ui-v2/src/app/app.module.ts
+++ rajkumar-tailor-ui-v2/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FeaturesComponent } from './features/features.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    FeaturesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
