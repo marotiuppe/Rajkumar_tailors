f44019b5605513a9f437f67c96d5832a2ac8a8cb
Maroti Uppe <marotiuppe891@gmail.com>
2023-10-10 13:01:35 +0530

    Updated dashboard card rendering to use dynamic data binding
    Changed the static HTML structure to a dynamic Angular template using *ngFor to loop through the dashboardCardList array. Each card's properties (background color, title, icon, count, and variation) are now bound to the corresponding values in the card object. This enhances code maintainability and allows for dynamic updates based on data.

--- rajkumar-tailor-dashboard/src/app/module/dashboard/dashboard.component.ts
+++ rajkumar-tailor-dashboard/src/app/module/dashboard/dashboard.component.ts
@@ -18,4 +18,28 @@ export class DashboardComponent {
       "assets/images/dashboard/Shirts.png",
     ]
   }
+
+  dashboardCardList:any=[
+    {
+      "title":"Total Orders",
+      "icon":"mdi mdi-diamond mdi-24px float-right",
+      "count":"95,5741",
+      "variation":"Increased by 5%",
+      "bg":"bg-gradient-info"
+    },
+    {
+      "title":"Pending Orders",
+      "icon":"mdi mdi-bookmark-outline mdi-24px float-right",
+      "count":"45,6334",
+      "variation":"Decreased by 10%",
+      "bg":"bg-gradient-success"
+    },
+    {
+      "title":"Recieved Payment",
+      "icon":"mdi mdi-diamond mdi-24px float-right",
+      "count":"15,0000",
+      "variation":"Increased by 60%",
+      "bg":"bg-gradient-danger"
+    }
+  ];
 }
